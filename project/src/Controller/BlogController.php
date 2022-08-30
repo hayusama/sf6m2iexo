e3fbbfbec337a7229a1cb033706d11108989e5e7
Anthony PARIS <contact@pixel-online.fr>
2022-08-30 17:20:35 +0200

    ok

--- project/src/Controller/BlogController.php
+++ project/src/Controller/BlogController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
+
+;
 
 class BlogController extends AbstractController
 {
@@ -16,17 +20,27 @@ class BlogController extends AbstractController
 
     #[Route('/add', name:"article_add")]
     public function add():Response {
-        return new Response('<body><h1>Ajouter un article</h1></body>');
+        return $this->render("blog/ajout.html.twig");
     }
 
     #[Route('/article/{id}/{url}', name:"article_show", requirements: ['id' => "\d+", 'url'=>'.{1,}'])]
-    public function show($id,$url):Response {
-        return new Response("<body><h1>Lire article {$url}</h1></body>");
+    public function show($id,$url, Request $request, Environment $twig):Response {
+        $globals = $twig->getGlobals();
+
+        dump($globals['webmaster']);
+        $tabInfo = [
+            "id" => 8,
+            "nom" => "Dupond",
+            "prenom" => "Xavier",
+            "age" => 60,
+            "localisation" => "disparu"
+        ];
+        return $this->render("blog/lecture.html.twig", ['id' => $id, 'url'=> $url, 'tabInfo' => $tabInfo]);
     }
 
     #[Route('/edition/{id}', name:"article_edit", requirements: ['id' => "\d+"])]
     public function edit($id):Response {
-        return new Response("<body><h1>Modifier article {$id}</h1></body>");
+        return $this->render("blog/edition.html.twig", ['id' => $id]);
     }
 
     #[Route('/suppression/{id}', name:"article_delete", requirements: ['id' => "\d+"])]
